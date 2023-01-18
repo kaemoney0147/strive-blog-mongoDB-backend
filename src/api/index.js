fd88a9c80f0bd0491a541e8f48192342414118b9
abiodun <114649936+kaemoney0147@users.noreply.github.com>
2023-01-18 16:31:14 +0100

    comment added

--- src/api/index.js
+++ src/api/index.js
@@ -101,4 +101,184 @@ blogPostRouter.delete("/:blogPostId", async (req, res, next) => {
   }
 });
 
+blogPostRouter.post("/:blogPostId/comments", async (req, res, next) => {
+  try {
+    const comment = await BlogPostsModel.findById(req.params.blogPostId, {
+      _id: 0,
+    });
+    if (comment) {
+      const commentToInsert = { ...req.body, addedOn: new Date() };
+
+      const postWithComment = await BlogPostsModel.findByIdAndUpdate(
+        req.params.blogPostId,
+        { $push: { comments: commentToInsert } },
+        { new: true, runValidators: true }
+      );
+      if (postWithComment) {
+        res.send(postWithComment);
+      } else {
+        next(
+          createHttpError(
+            404,
+            `Post with id ${req.params.blogPostId} not found`
+          )
+        );
+      }
+    } else {
+      next(
+        createHttpError(404, `Post with id ${req.params.blogPostId} not found`)
+      );
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+blogPostRouter.get("/:blogPostId/comments", async (req, res, next) => {
+  try {
+    const post = await BlogPostsModel.findById(req.params.postid);
+    if (post) {
+      res.send(post.comments);
+    } else {
+      next(createHttpError(404, `Post with id ${req.params.id} not found.`));
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+blogPostRouter.get(
+  "/:blogPostId/comments/:commentid",
+  async (req, res, next) => {
+    try {
+      const post = await BlogPostsModel.findById(req.params.blogPostId);
+      if (post) {
+        const postComment = post.comments.find(
+          (comment) => comment._id.toString() === req.params.commentid
+        );
+
+        if (postComment) {
+          res.send(postComment);
+        } else {
+          next(
+            createHttpError(
+              404,
+              `Comment with id ${req.params.commentid} not found.`
+            )
+          );
+        }
+      } else {
+        next(
+          createHttpError(
+            404,
+            `Comment with id ${req.params.commentid} not found.`
+          )
+        );
+      }
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+blogPostRouter.get("/:blogPostId", async (req, res, next) => {
+  try {
+    const post = await BlogPostsModel.findById(req.params.blogPostId);
+    if (post) {
+      res.send(post);
+    } else {
+      next(
+        createHttpError(404, `Post with id ${req.params.blogPostId} not found.`)
+      );
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+blogPostRouter.put("/:blogPostId", async (req, res, next) => {
+  try {
+    const updatedPost = await BlogPostsModel.findByIdAndUpdate(
+      req.params.blogPostId,
+      req.body,
+      { new: true, runValidators: true }
+    );
+
+    if (updatedPost) {
+      res.send(updatedPost);
+    } else {
+      next(
+        createHttpError(404, `Post with id ${req.params.blogPostId} not found.`)
+      );
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+blogPostRouter.put(
+  "/:blogPostId/comments/:commentid",
+  async (req, res, next) => {
+    try {
+      const post = await BlogPostsModel.findById(req.params.blogPostId);
+      if (post) {
+        const index = post.comments.findIndex(
+          (comment) => comment._id.toString() === req.params.commentid
+        );
+        if (index !== -1) {
+          post.comments[index] = {
+            ...post.comments[index].toObject(),
+            ...req.body,
+            updatedOn: new Date(),
+          };
+          await post.save();
+          res.send(post);
+        }
+      } else {
+        next(createHttpError(404, `Comment not found`));
+      }
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+blogPostRouter.delete("/:blogPostId", async (req, res, next) => {
+  try {
+    const deletedPost = await BlogPostsModel.findByIdAndDelete(
+      req.params.blogPostId
+    );
+    if (deletedPost) {
+      res.status(204).send();
+    } else {
+      next(
+        createHttpError(
+          404,
+          `Cannot delete post with id ${req.params.blogPostId} as it does not exist`
+        )
+      );
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+blogPostRouter.delete(
+  "/:blogPostId/comments/:commentid",
+  async (req, res, next) => {
+    try {
+      const updatedPost = await BlogPostsModel.findByIdAndUpdate(
+        req.params.blogPostId,
+        { $pull: { comments: { _id: req.params.commentid } } },
+        { new: true }
+      );
+      if (updatedPost) {
+        res.send(updatedPost);
+      } else {
+        next(createHttpError(404, `Post not found`));
+      }
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
 export default blogPostRouter;
--- src/api/model.js
+++ src/api/model.js
@@ -6,19 +6,24 @@ const blogPostsSchema = new Schema(
   {
     category: { type: String, required: true },
     title: { type: String, required: true },
-    cover: { type: String, required: true },
+    cover: { type: String, required: false },
     readTime: {
-      value: { type: Number },
+      value: { type: Number, required: true },
+      unit: { type: String, required: true },
     },
     author: {
-      name: { type: String },
-      avatar: { type: String },
+      name: { type: String, required: false },
+      avatar: { type: String, required: false },
     },
     content: { type: String, required: true },
+    comments: [
+      {
+        comment: String,
+        addedOn: Date,
+        updatedOn: Date,
+      },
+    ],
   },
-  {
-    timestamps: true, // this option automatically the createdAt and updatedAt fields
-  }
+  { timestamps: true }
 );
-
 export default model("BlogPost", blogPostsSchema); // this model is now automagically linked to the "blogPosts" collection, if collection is not there it will be created
